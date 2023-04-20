de30a381889cb8654910090a9909d12902da6da9
kazak25 <kazak77709@gmail.com>
2023-04-20 21:27:44 +0300

    Update #1

--- Assets/Scripts/Configs/ConfigSystem.cs
+++ Assets/Scripts/Configs/ConfigSystem.cs
@@ -3,7 +3,25 @@ using System.Collections.Generic;
 using ScriptableObjects;
 using UnityEngine;
 
-public class ConfigSystem : MonoBehaviour
+public class KitchenZoneInitializer : MonoBehaviour
 {
-    [SerializeField] private LevelsConfigCollection _levelsConfigCollection;
+    [SerializeField] private KitchenZonesConfigCollection _kitchenZonesConfigCollection;
+
+    private KitchenZoneModel[] _kitchenZoneModels;
+    
+    public void CreateKitchenModels()
+    {
+        _kitchenZoneModels = new KitchenZoneModel[_kitchenZonesConfigCollection.ConfigKitchenZoneModels.Length];
+
+        for (var i = 0; i < _kitchenZonesConfigCollection.ConfigKitchenZoneModels.Length; i++)
+        {
+            var kitchenZone = _kitchenZonesConfigCollection.ConfigKitchenZoneModels[i];
+            //var businessImproves = CreateBusinessImproveModels(i);
+
+          //  _kitchenZoneModels[i] = new KitchenZoneModel(kitchenZone.NameZone,kitchenZone.StartPositions,kitchenZone.Prefab, 
+              //  kitchenZone.ConfigImprovement,kitchenZone.ConfigItem);
+        }
+    }
+
+    
 }
