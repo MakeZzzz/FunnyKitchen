bb8c903d2dbc385a2ba6a110abb43814bcb17714
Daniil <74076416+DaniilDongiv@users.noreply.github.com>
2023-05-01 21:02:37 +0300

    Update is Scene

--- Assets/Scripts/Configs/ConfigImprovementModel.cs
+++ Assets/Scripts/Configs/ConfigImprovementModel.cs
@@ -1,11 +1,12 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable]
-public class ConfigImprovementModel
+namespace Configs
 {
-    public int Price => _price;
-    [SerializeField] private int _price;
+    [Serializable]
+    public class ConfigImprovementModel
+    {
+        public int Price => _price;
+        [SerializeField] private int _price;
+    }
 }
